1bd06a5e0ce87fd298bbe4b58b01eb0fbbc17fef
Igor <buranovigor@gmail.com>
2018-05-14 10:34:57 +0300

    Change exports style in validators.js

--- backend/forms/validators.js
+++ backend/forms/validators.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const length = (value, minLength, maxLength) => value.length >= minLength && value.length <= maxLength;
+exposts.length = (value, minLength, maxLength) => value.length >= minLength && value.length <= maxLength;
 
-module.exports = {
-	length: length,
-	email: (value) => throw('Not implemented'),
-	greaterOrEqual: (value, a) => value >= a,
-	after: (value, date) => throw('Not implemented')
-};
+exports.email = (value) => throw('Not implemented');
+
+exports.greaterOrEqual = (value, a) => value >= a;
+
+exports.after = (value, date) => throw('Not implemented');
