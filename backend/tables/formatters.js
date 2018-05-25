a25a38c853ff4584a6eb6b3135b1e44403c9aa9b
Igor <buranovigor@gmail.com>
2018-05-25 16:42:13 +0300

    Add frontend formatters

--- backend/tables/formatters.js
+++ backend/tables/formatters.js
@@ -1,4 +1,36 @@
 'use strict';
 
-exports.departmentLink = row => `<a href="/departments/${row.d_id}">${row.d_title}</a>`;
+// Formatter returns:
+// plain string - should be rendered as text
+// object {html: "..."} - should be rendered as html
 
+
+/*exports.departmentLink = () => row => {
+	html: `<a href="/departments/${row.d_id}">${row.d_title}</a>`
+};
+exports.paramsTest = (x, y) => row => `${x} + ${y} = ${x+y}`;*/
+
+
+exports.departmentLink = {
+	params: [],
+	f: function() {
+		return function(row) {
+			return {
+				html:
+					'<a href="/departments/' +
+					row.d_id + 
+					'">' + 
+					row.d_title + 
+					'</a>'
+			}
+		};
+	}
+}
+exports.paramsTest = {
+	params: ["x", "y"],
+	f: function(x, y) {
+		return function(row) {
+			return x + ' + ' + y + ' = ' + (x+y);
+		}
+	}
+}
