bbf831c1563dbea393aba3e7e658a9c2a1efd2ee
Igor <buranovigor@gmail.com>
2018-06-01 12:10:40 +0300

    Add global error handling

--- backend/index.js
+++ backend/index.js
@@ -12,53 +12,68 @@ const { DB, HTTP, FORM } = require('core/index');
 
 //main handler
 exports.handler = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+	const handleFatalError = (err) => {
+		console.log(err);
+		callback(null, HTTP.response(500, {
+			error: (process.env.PROD === 'true' ? 'Something went wrong' : {
+				message: err.message,
+				stack: err.stack
+			}) 
+		}));
+		process.exit(1);
+	};
+	
+	process.on('unhandledRejection', (reason, p) => {
+		handleFatalError(reason);
+	});
+	
+	try {
+		context.callbackWaitsForEmptyEventLoop = false;
 
-    let api;
-    const [resource, action] = event.pathParameters['proxy'].split('/');
-    const method = event.httpMethod;
+		let api;
+		const [resource, action] = event.pathParameters['proxy'].split('/');
+		const method = event.httpMethod;
 
-    //OPTIONS requests are proccessed by API GateWay using mock
-    //sam-local can't do it, so for local development we need this 
-    if (method === 'OPTIONS') {
-        return callback(null, HTTP.response());
-    }
+		//OPTIONS requests are proccessed by API GateWay using mock
+		//sam-local can't do it, so for local development we need this 
+		if (method === 'OPTIONS') {
+			return callback(null, HTTP.response(200));
+		}
 
-    //require resource module
-    try {
-        api = require('api/' + resource)();
-    } catch (e) {
-        if (e.code === 'MODULE_NOT_FOUND') {
-            return callback(null, HTTP.response(404, { error: 'Resource not found.' }));
-        }
-        return callback(null, HTTP.response(500));
-    }
+		//require resource module
+		try {
+			api = require('api/' + resource)();
+		} catch (e) {
+			if (e.code === 'MODULE_NOT_FOUND') {
+				return callback(null, HTTP.response(404, { error: 'Resource not found.' }));
+			}
+			throw e;
+		}
 
-    //call resource action
-    if (api.hasOwnProperty(action)) {
-        //check token for protected action
-        if (api[action].protected === 1) {
-            if (event.headers['X-Access-Token'] === undefined) {
-                return callback(null, HTTP.response(403, { error: 'No token provided.' }));
-            }
-            try {
-                event.userData = jwt.verify(token, process.env.SECRET);
-            } catch (error) {
-                return callback(null, HTTP.response(403, { error: 'Failed to verify token.' }));
-            }
-        }
+		//call resource action
+		if (api.hasOwnProperty(action)) {
+			//check token for protected action
+			if (api[action].protected === 1) {
+				if (event.headers['X-Access-Token'] === undefined) {
+					return callback(null, HTTP.response(403, { error: 'No token provided.' }));
+				}
+				try {
+					event.userData = jwt.verify(token, process.env.SECRET);
+				} catch (error) {
+					return callback(null, HTTP.response(403, { error: 'Failed to verify token.' }));
+				}
+			}
 
-        // check if this method is not allowed
-        if (!api[action].hasOwnProperty(method)) {
-            return callback(null, HTTP.response(405), { error: 'Method not allowed.' });
-        }
+			// check if this method is not allowed
+			if (!api[action].hasOwnProperty(method)) {
+				return callback(null, HTTP.response(405), { error: 'Method not allowed.' });
+			}
 
-        //finally call the api
-        try {
-            return api[action][method](event, context, callback);
-        } catch (error) {
-            return callback(null, HTTP.response(500, { error: (process.env.PROD ? 'Something went wrong' : error) }));
-        }
-    }
-    return callback(null, HTTP.response(404, { error: 'Action not found.' }));
+			//finally call the api
+			return api[action][method](event, context, callback);
+		}
+		return callback(null, HTTP.response(404, { error: 'Action not found.' }));
+	} catch(err) {
+		handleFatalError(err);
+	}
 }
