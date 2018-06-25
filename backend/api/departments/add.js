c09fea7ac07c04347cd320043dde32572c98d4eb
Igor <buranovigor@gmail.com>
2018-06-25 20:45:03 +0300

    Temporarily set 'protected' = 0 on all actions to avoid token check bug

--- backend/api/departments/add.js
+++ backend/api/departments/add.js
@@ -13,7 +13,7 @@ module.exports.GET = (event, context, callback) => {
 	FORM.getAsObject('department', [0])
 		.then(fields => callback(null, HTTP.response(200, fields)));
 };
-module.exports.GET.protected = 1;
+module.exports.GET.protected = 0;
 
 //Method: POST
 //Params: d_title, d_head, d_size, d_created
@@ -37,4 +37,4 @@ module.exports.POST = (event, context, callback) => {
 				.then(() => callback(null, HTTP.response(200)));
 		});
 };
-module.exports.POST.protected = 1;
+module.exports.POST.protected = 0;
